4e96def1a7f6c261f48ec7a8c192d8d33e0a3206
Kamule <benjamin.herbomez@gmail.com>
2019-04-28 00:25:52 +0200

    FIX unit tests

--- test/controller/user.controller.spec.ts
+++ test/controller/user.controller.spec.ts
@@ -1,53 +1,54 @@
-import UserController  from '../../app/controllers/user.controller';
+import { createUser, getUser }  from "../../app/controllers/user.controller";
 import { User } from '../../app/models/user.model';
 import {BaseError, ErrorCodes} from '../../app/exceptions/base-error';
 
 
 jest.mock('../../app/models/user.model');
 
-describe("User controller", () => {
-    
-    it('Empty username to throw validation error', async () => {
-        await expect(UserController.createUser({email:"", username:""})).rejects.toEqual(
-            new BaseError("Username is invalid", ErrorCodes.INVALID_USERNAME)
-        );
-    });
-    it('Empty email to throw validation error', async () => {
-        await expect(UserController.createUser({email:"", username:"test"})).rejects.toEqual(
-            new BaseError("This email is invalid", ErrorCodes.INVALID_EMAIL)
-        );
-    });
-    it('Malformated email to throw validation error', async () => {
-        await expect(UserController.createUser({email:"testqovbzi", username:"test"})).rejects.toEqual(
-            new BaseError("This email is invalid", ErrorCodes.INVALID_EMAIL)
-        );
-    });
+describe("User controller", () => {     
+    describe("Create user", () => {
+        it('Empty username to throw validation error', async () => {
+            await expect(createUser({email:"", username:""})).rejects.toEqual(
+                new BaseError("Username is invalid", ErrorCodes.INVALID_USERNAME)
+            );
+        });
+        it('Empty email to throw validation error', async () => {
+            await expect(createUser({email:"", username:"test"})).rejects.toEqual(
+                new BaseError("This email is invalid", ErrorCodes.INVALID_EMAIL)
+            );
+        });
+        it('Malformated email to throw validation error', async () => {
+            await expect(createUser({email:"testqovbzi", username:"test"})).rejects.toEqual(
+                new BaseError("This email is invalid", ErrorCodes.INVALID_EMAIL)
+            );
+        });
 
-    it('Existing username or email should throw an error', async () => {
-        User.findOne = jest.fn().mockImplementation((param)=>{
-            return Promise.resolve({
+        it('Existing username or email should throw an error', async () => {
+            User.findOne = jest.fn().mockImplementation((param)=>{
+                return Promise.resolve({
+                    username:"toto",
+                    email:"toto@email.com"
+                });
+            });
+            await expect(createUser({email:"test@email.com", username:"test"})).rejects.toEqual(
+                new BaseError("Username or email already exists", ErrorCodes.EXISTING_ACCOUNT)
+            );
+        });
+
+        it('When all good, user created', async () => {
+            User.findOne = jest.fn().mockImplementation((param)=>{
+                return Promise.resolve(null);
+            });
+            User.create = jest.fn().mockImplementation((param)=>{
+                return Promise.resolve({
+                    username:"toto",
+                    email:"toto@email.com"
+                });
+            });
+            await expect(createUser({email:"test@email.com", username:"test"})).resolves.toEqual({
                 username:"toto",
                 email:"toto@email.com"
             });
-        });
-        await expect(UserController.createUser({email:"test@email.com", username:"test"})).rejects.toEqual(
-            new BaseError("Username or email already exists", ErrorCodes.EXISTING_ACCOUNT)
-        );
-    });
-
-    it('When all good, user created', async () => {
-        User.findOne = jest.fn().mockImplementation((param)=>{
-            return Promise.resolve(null);
-        });
-        User.create = jest.fn().mockImplementation((param)=>{
-            return Promise.resolve({
-                username:"toto",
-                email:"toto@email.com"
-            });
-        });
-        await expect(UserController.createUser({email:"test@email.com", username:"test"})).resolves.toEqual({
-            username:"toto",
-            email:"toto@email.com"
         });
     });
 })
